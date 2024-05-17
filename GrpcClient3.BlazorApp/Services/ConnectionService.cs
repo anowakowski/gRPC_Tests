6621c7ee95c7f65205c340f55c6dd9a05323bcff
Adrian Nowakowski <adrian.nowakowski@nokia.com>
2024-05-17 16:09:38 +0200

    added blazor app

---
+++ GrpcClient3.BlazorApp/Services/ConnectionService.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcClient3.BlazorApp.Services
+{
+    public class ConnectionService
+    {
+        public void ChceckIfClientIsInstalledOnMachine()
+        {
+            //gRPC call
+        }
+    }
+}
